8be4e4361f7292b7e0e6e14d7ddb296997ade9b4
Seongmin Park <wisesunday96@gmail.com>
2023-02-24 22:23:26 +0900

    let 변수선언

---
+++ JS 공부/script.js
@@ -0,0 +1,11 @@
+let plus = 10;
+console.log(plus-1); //9
+plus = 5;
+console.log(plus-1); //4
+
+//4개의 hello를 한번에 변경가능(=변수 재사용)
+let greet = "Hello";
+console.log(greet);
+console.log(greet);
+console.log(greet);
+console.log(greet);
