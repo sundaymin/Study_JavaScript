308a6ea47af2ddb99108f79a3cfb0cfaed063fbc
Seongmin Park <wisesunday96@gmail.com>
2023-02-26 22:01:52 +0900

    객체와 생성자 함수

--- JS 공부/script.js
+++ JS 공부/script.js
@@ -1,11 +1,15 @@
-const select = document.querySelector('select');
-const button = document.querySelector('button');
+function Dog(paramName, paramBreed){
+    this.name = paramName
+    this.breed =paramBreed
+}
+//생성자함수 매개변수가 2개니까, 객체 생성시에도 인자를 2개 작성
+const dog1 = new Dog("방울", "시츄");
+const dog2 = new Dog("치즈","웰시코기");
+const dog3 = new Dog("버터","푸들");
+const dog4 = new Dog("만두","말티푸");
 
-button.addEventListener('click',function(){
-    console.log(select.value);//셀렉트태그의 값을 읽어옴
-    let result;
-    result = select.value == "foot"?
-    "축구를 선택함" :
-    "축구를 안선택함"
-    alert(result);
-})
+console.log(dog1);
+console.log(dog2);
+console.log(dog3);
+console.log(dog4);
+
