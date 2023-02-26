49fab9fb029b50e568b487e5ab91de94e92dc760
Seongmin Park <wisesunday96@gmail.com>
2023-02-27 01:37:04 +0900

    classList

--- JS 공부/script.js
+++ JS 공부/script.js
@@ -1,15 +1,16 @@
-function Dog(paramName, paramBreed){
-    this.name = paramName
-    this.breed =paramBreed
-}
-//생성자함수 매개변수가 2개니까, 객체 생성시에도 인자를 2개 작성
-const dog1 = new Dog("방울", "시츄");
-const dog2 = new Dog("치즈","웰시코기");
-const dog3 = new Dog("버터","푸들");
-const dog4 = new Dog("만두","말티푸");
+const h1 = document.querySelector('h1');
+const addBtn = document.querySelector('#add');
+const removeBtn = document.querySelector('#remove');
+const toggleBtn = document.querySelector('#toggle');
 
-console.log(dog1);
-console.log(dog2);
-console.log(dog3);
-console.log(dog4);
+console.log(h1.classList);
 
+addBtn.addEventListener('click',function(){
+    h1.classList.add('text');
+})
+removeBtn.addEventListener('click',function(){
+    h1.classList.remove('text');
+})
+toggleBtn.addEventListener('click',function(){
+    h1.classList.toggle('text');
+})
