267cd2c24db2bd93a2bad49c6e287ac6be3c0d17
Seongmin Park <wisesunday96@gmail.com>
2023-02-27 23:12:04 +0900

    Math 객체

--- JS 공부/script.js
+++ JS 공부/script.js
@@ -1,7 +1,6 @@
-const pet = ["강아지","고양이","앵무새","햄스터"];
+let number = 100;
 
-console.log(pet);
-pet.forEach(function(param){
-    console.log(param);//배열의 요소를 각각 실행함
-});
+console.log(Math.pow(10, 4));//100의 5제곱(10000)
 
+let powNum = (Math.pow(10, 4));
+console.log(Math.sqrt(powNum));//10000의 제곱근(100)
