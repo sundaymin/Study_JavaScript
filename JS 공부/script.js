be3a69f209041357ffa476323b4b06a3d56d9cd9
Seongmin Park <wisesunday96@gmail.com>
2023-03-01 01:25:38 +0900

    객체리터럴

--- JS 공부/script.js
+++ JS 공부/script.js
@@ -1,6 +1,13 @@
-let number = 100;
-
-console.log(Math.pow(10, 4));//100의 5제곱(10000)
-
-let powNum = (Math.pow(10, 4));
-console.log(Math.sqrt(powNum));//10000의 제곱근(100)
+const cat = {
+    name : "tom",
+    color : "gray",
+    eat : function(){
+        console.log('냠냠');
+    },
+    sound : function(meow){
+        console.log(meow);
+    }
+}
+console.log(cat.name);//tom (값 읽기 가능)
+cat.name = "Navi"
+console.log(cat.name);//Navi (값 변경도 가능)
